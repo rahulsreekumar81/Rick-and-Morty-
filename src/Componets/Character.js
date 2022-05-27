eb15c07ba45c7a145394800070693e48ebb64062
Rahul <rahulsreekumar81@gmail.com>
2022-05-27 22:05:04 +0530

    first commit

---
+++ src/Componets/Character.js
@@ -0,0 +1,14 @@
+
+export default function Character({ character }) {
+  return (
+    <div className="card">
+        <img src={character.image} alt={character.name} />
+        <div className="text-container">
+            <h3>{character.name}</h3>
+            <p className="status">{character.status} - {character.species} </p>
+            <p className="title"> Last seen on</p>
+            <p>{character.location.name}</p>
+        </div> 
+    </div>
+  )
+}
